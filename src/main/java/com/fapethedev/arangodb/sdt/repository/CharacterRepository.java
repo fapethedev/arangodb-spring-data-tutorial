421cb5dee49dcd43540f897ce50c60708b84adf4
fapethedev <mpetrivlin@gmail.com>
2024-07-11 22:37:29 +0000

    ArangoDB SpringData Tutorial:  CRUD Operation:   Create a repository

---
+++ src/main/java/com/fapethedev/arangodb/sdt/repository/CharacterRepository.java
@@ -0,0 +1,5 @@
+package com.fapethedev.arangodb.sdt.repository;
+
+import com.arangodb.springframework.repository.ArangoRepository;
+
+public interface CharacterRepository extends ArangoRepository<Character, String> {}
