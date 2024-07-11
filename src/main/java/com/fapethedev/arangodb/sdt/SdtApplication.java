b37b0bbbe392798f17847ff33e0f9094a5d86d6e
fapethedev <mpetrivlin@gmail.com>
2024-07-11 22:18:38 +0000

    ArangoDB SpringData Tutorial:  Create an Application class

--- src/main/java/com/fapethedev/arangodb/sdt/SdtApplication.java
+++ src/main/java/com/fapethedev/arangodb/sdt/SdtApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SdtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SdtApplication.class, args);
+		Class<?>[] runner = new Class<?>[]{};
+        System.exit(SpringApplication.exit(SpringApplication.run(runner, args)));
 	}
 
 }
