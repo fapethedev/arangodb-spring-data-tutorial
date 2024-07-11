a0c33a6a6108fba853daa28027520c7b8a7eab5d
fapethedev <mpetrivlin@gmail.com>
2024-07-11 22:14:40 +0000

    ArangoDB SpringData Tutorial Init Commit

---
+++ src/main/java/com/fapethedev/arangodb/sdt/SdtApplication.java
@@ -0,0 +1,13 @@
+package com.fapethedev.arangodb.sdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdtApplication.class, args);
+	}
+
+}
