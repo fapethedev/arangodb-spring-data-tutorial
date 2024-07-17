358ec8338d8b192d535451d3c6e6f6b7f7cd0681
fapethedev <mpetrivlin@gmail.com>
2024-07-17 14:16:54 +0000

    ArangoDB SpringData Tutorial:  Query By Example:   Multiple entities

--- src/main/java/com/fapethedev/arangodb/sdt/runner/ByExampleRunner.java
+++ src/main/java/com/fapethedev/arangodb/sdt/runner/ByExampleRunner.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.GenericPropertyMatcher;
 
 @RequiredArgsConstructor
 @ComponentScan("com.fapethedev.arangodb.sdt")
@@ -22,5 +24,26 @@ public class ByExampleRunner implements CommandLineRunner
         var foundNedStark = repository.findOne(Example.of(nedStark));
         assert foundNedStark.isPresent();
         System.out.printf("Found %s%n", foundNedStark.get());
+
+        System.out.println("## Find all dead Starks");
+        var allDeadStarks = repository
+                .findAll(Example.of(new Character(null, "Stark", false), ExampleMatcher.matchingAll()
+                        .withMatcher("surname", GenericPropertyMatcher::exact).withIgnorePaths("name", "age")));
+        allDeadStarks.forEach(System.out::println);
+
+        System.out.println("## Find all Starks which are 30 years younger than Ned Stark");
+        Iterable<Character> allYoungerStarks = repository.findAll(
+                Example.of(foundNedStark.get(), ExampleMatcher.matchingAll()
+                        .withMatcher("surname", GenericPropertyMatcher::exact)
+                        .withIgnorePaths("id", "name", "alive")
+                        .withTransformer("age", age -> age.map(it -> (int) it - 30))));
+        allYoungerStarks.forEach(System.out::println);
+
+        System.out.println("## Find all character which surname ends with 'ark' (ignore case)");
+        Iterable<Character> ark = repository.findAll(Example.of(new Character(null, "ark", false),
+                ExampleMatcher.matchingAll().withMatcher("surname", GenericPropertyMatcher::endsWith)
+                        .withIgnoreCase()
+                        .withIgnorePaths("name", "alive", "age")));
+        ark.forEach(System.out::println);
     }
 }
