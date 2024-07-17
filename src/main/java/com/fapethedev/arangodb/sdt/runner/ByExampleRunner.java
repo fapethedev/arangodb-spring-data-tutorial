1e65f462c84656f9632f3d157bab8f65db561372
fapethedev <mpetrivlin@gmail.com>
2024-07-17 14:13:20 +0000

    ArangoDB SpringData Tutorial:  Query By Example:   Single entity

--- src/main/java/com/fapethedev/arangodb/sdt/runner/ByExampleRunner.java
+++ src/main/java/com/fapethedev/arangodb/sdt/runner/ByExampleRunner.java
@@ -1,9 +1,11 @@
 package com.fapethedev.arangodb.sdt.runner;
 
+import com.fapethedev.arangodb.sdt.entity.Character;
 import com.fapethedev.arangodb.sdt.repository.CharacterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Example;
 
 @RequiredArgsConstructor
 @ComponentScan("com.fapethedev.arangodb.sdt")
@@ -14,5 +16,11 @@ public class ByExampleRunner implements CommandLineRunner
     @Override
     public void run(String... args) throws Exception {
         System.out.println("# Query by example");
+
+        var nedStark = new Character("Ned", "Stark", false, 41);
+        System.out.printf("## Find character which exactly match %s%n", nedStark);
+        var foundNedStark = repository.findOne(Example.of(nedStark));
+        assert foundNedStark.isPresent();
+        System.out.printf("Found %s%n", foundNedStark.get());
     }
 }
